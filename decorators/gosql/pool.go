da684184efbce125b48c3b6f62f3c35b5c63cdfd
Maxim Eryomenko <moeryomenko@gmail.com>
2022-07-16 21:54:55 +0300

    perf(gosql): ping with enough idle connections
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

--- decorators/gosql/pool.go
+++ decorators/gosql/pool.go
@@ -11,11 +11,16 @@ import (
 
 var ErrPoolNotReady = errors.New("currently pool is busy")
 
-const defaultPingInterval = 500 * time.Millisecond
+const (
+	defaultPingInterval = 500 * time.Millisecond
+
+	defaultPingPolicy = 5
+)
 
 type SqlController struct {
 	db           *sql.DB
 	pingInterval time.Duration
+	lowerLimit   int
 }
 
 // New returns new instance of sql.DB readiness controller.
@@ -34,15 +39,23 @@ func New(_ context.Context, db *sql.DB, opts ...Option) *SqlController {
 func (c *SqlController) CheckReadinessProbe(ctx context.Context) healing.CheckResult {
 	return healing.CheckHelper(ctx, c.pingInterval, func() error {
 		stats := c.db.Stats()
-		switch stats.MaxOpenConnections - stats.InUse {
-		case 0:
-			return ErrPoolNotReady
-		default:
-			// NOTE: if the value is non-zero, then this means that
-			// the connection pool has the ability to allocate
-			// a connection and perform a ping.
+		if stats.MaxOpenConnections == 0 {
 			return c.db.PingContext(ctx)
 		}
+
+		ratioFree := (stats.MaxOpenConnections - stats.InUse) / stats.MaxOpenConnections * 100
+
+		if ratioFree == 0 {
+			return ErrPoolNotReady
+		}
+
+		if ratioFree > c.lowerLimit {
+			return c.db.PingContext(ctx)
+		}
+
+		// NOTE: don't have enough free connections
+		// that will be useful for processing incoming requests.
+		return nil
 	})
 }
 
@@ -52,5 +65,13 @@ type Option func(*SqlController)
 func WithPingInterval(interval time.Duration) Option {
 	return func(sc *SqlController) {
 		sc.pingInterval = interval
+	}
+}
+
+// WithLowerLimitPool sets the lower limit of free connections
+// during which a real ping of the base is performed.
+func WithLowerLimitPool(lower uint) Option {
+	return func(sc *SqlController) {
+		sc.lowerLimit = int(lower)
 	}
 }
